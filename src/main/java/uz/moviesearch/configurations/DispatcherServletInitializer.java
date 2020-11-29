f487381a0fcbf83168960c26ea248214b3c1219e
Tuychiyev Aziz <aziz_168@mail.ru>
2020-11-30 03:57:59 +0500

    ready to deploy

--- src/main/java/uz/moviesearch/configurations/DispatcherServletInitializer.java
+++ src/main/java/uz/moviesearch/configurations/DispatcherServletInitializer.java
@@ -1,6 +1,6 @@
 package uz.moviesearch.configurations;
 
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter .CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
